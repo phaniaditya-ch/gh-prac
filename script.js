d53d695908f94d0847a02673af0066fcc0885678
phaniaditya-ch <chphaniaditya@gmail.com>
2024-05-10 21:37:56 +0530

    script added

---
+++ script.js
@@ -0,0 +1,25 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+    const sqrtNum = Math.sqrt(num);
+    for (let i = 3; i <= sqrtNum; i += 2) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function printFirstNPrimes(n) {
+    const primes = [];
+    let num = 2;
+    while (primes.length < n) {
+        if (isPrime(num)) {
+            primes.push(num);
+        }
+        num++;
+    }
+    console.log(primes.join(', '));
+}
+
+const n = 10; // Change this to the desired number of prime numbers you want to print
+printFirstNPrimes(n);
